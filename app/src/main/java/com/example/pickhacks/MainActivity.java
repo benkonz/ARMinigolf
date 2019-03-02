e3270eefe5ba6d53027ba5c4ae80e87f5ba38d39
pgayee694 <pgayee694@gmail.com>
2019-03-02 15:44:06 -0600

    Made testing map

--- app/src/main/java/com/example/pickhacks/MainActivity.java
+++ app/src/main/java/com/example/pickhacks/MainActivity.java
@@ -15,6 +15,8 @@ import com.google.ar.core.Anchor;
 import com.google.ar.core.HitResult;
 import com.google.ar.core.Plane;
 import com.google.ar.sceneform.AnchorNode;
+import com.google.ar.sceneform.Node;
+import com.google.ar.sceneform.collision.Box;
 import com.google.ar.sceneform.math.Vector3;
 import com.google.ar.sceneform.rendering.Color;
 import com.google.ar.sceneform.rendering.MaterialFactory;
@@ -29,7 +31,10 @@ public class MainActivity extends AppCompatActivity {
     private static final double MIN_OPENGL_VERSION = 3.0;
     ArFragment arFragment;
     ModelRenderable sphere;
-    boolean hasSpawned = false;
+    ModelRenderable wall;
+    Box[] map;
+    boolean hasSpawnedBall = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,11 +45,39 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         arFragment = (ArFragment) getSupportFragmentManager().findFragmentById(R.id.ux_fragment);
 
-        MaterialFactory.makeOpaqueWithColor(this, new Color(android.graphics.Color.RED))
+        map = new Box[61];
+        for (int i = 0; i < 30; i++) {
+            map[i] = new Box();
+            map[i].setSize(new Vector3(0.2f, 0.15f, 0.2f));
+            map[i].setCenter(new Vector3(-0.4f, 0.0f, -0.04f * i));
+        }
+        for (int i = 30; i < 60; i++) {
+            map[i] = new Box();
+            map[i].setSize(new Vector3(0.2f, 0.15f, 0.2f));
+            map[i].setCenter(new Vector3(0.4f, 0.0f, -0.04f * (i % (map.length/2))));
+        }
+        map[60] = new Box();
+        map[60].setSize(new Vector3(1f, 0.15f, 0.2f));
+        map[60].setCenter(new Vector3(0.0f, 0.0f, -1.2f));
+
+
+
+        MaterialFactory.makeOpaqueWithColor(this, new Color(android.graphics.Color.WHITE))
                 .thenAccept(
                         material -> {
                             sphere =
-                                    ShapeFactory.makeSphere(0.1f, new Vector3(0.0f, 0.15f, 0.0f), material); }).exceptionally(throwable -> {
+                                    ShapeFactory.makeSphere(0.1f, new Vector3(0.0f, 0f, 0.0f), material); }).exceptionally(throwable -> {
+                    Toast toast = Toast.makeText(this, "Unable draw Shape", Toast.LENGTH_LONG);
+                    toast.setGravity(Gravity.CENTER, 0, 0);
+                    toast.show();
+                    return null;
+                });
+
+        MaterialFactory.makeOpaqueWithColor(this, new Color(android.graphics.Color.BLACK))
+                .thenAccept(
+                        material -> {
+                            wall =
+                                    ShapeFactory.makeCube(map[0].getSize(), map[0].getCenter(), material); }).exceptionally(throwable -> {
             Toast toast = Toast.makeText(this, "Unable draw Shape", Toast.LENGTH_LONG);
             toast.setGravity(Gravity.CENTER, 0, 0);
             toast.show();
@@ -52,13 +85,14 @@ public class MainActivity extends AppCompatActivity {
         });
 
         arFragment.setOnTapArPlaneListener((HitResult hitResult, Plane plane, MotionEvent motionEvent) ->
+
         {
             if (sphere == null)
             {
                 return;
             }
 
-            if (!hasSpawned)
+            if (!hasSpawnedBall)
             {
                 Anchor anchor = hitResult.createAnchor();
                 AnchorNode anchorNode = new AnchorNode(anchor);
@@ -68,9 +102,45 @@ public class MainActivity extends AppCompatActivity {
                 s.setParent(anchorNode);
                 s.setRenderable(sphere);
                 s.select();
-                hasSpawned = true;
-            }
 
+                Anchor wallAnchor = hitResult.createAnchor();
+                AnchorNode wallAnchorNode = new AnchorNode(wallAnchor);
+                wallAnchorNode.setParent(arFragment.getArSceneView().getScene());
+
+                for (int i =0; i < map.length; i++) {
+                    final int pos = i;
+                    MaterialFactory.makeOpaqueWithColor(this, new Color(android.graphics.Color.BLACK))
+                            .thenAccept(
+                                    material -> {
+                                        wall =
+                                                ShapeFactory.makeCube(map[pos].getSize(), map[pos].getCenter(), material); }).exceptionally(throwable -> {
+                        Toast toast = Toast.makeText(this, "Unable draw Shape", Toast.LENGTH_LONG);
+                        toast.setGravity(Gravity.CENTER, 0, 0);
+                        toast.show();
+                        return null;
+                    });
+
+                    Node n = new Node();
+                    n.setParent(wallAnchorNode);
+                    n.setRenderable(wall);
+                }
+                hasSpawnedBall = true;
+            }
+            /**else {
+
+                if (wall == null)
+                {
+                    return;
+                }
+
+                Anchor wallAnchor = hitResult.createAnchor();
+                AnchorNode wallAnchorNode = new AnchorNode(wallAnchor);
+                wallAnchorNode.setParent(arFragment.getArSceneView().getScene());
+
+                Node n = new Node();
+                n.setParent(wallAnchorNode);
+                n.setRenderable(wall);
+            }*/
         });
 
     }
