644da9e3e916dbbf025e5b7717cfe1c3bd03a3f6
Brandon Lacy <blacy1551@gmail.com>
2019-03-02 12:20:31 -0600

    Created helper function for spawning sphere

--- app/src/main/java/com/example/pickhacks/MainActivity.java
+++ app/src/main/java/com/example/pickhacks/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.pickhacks;
 import android.app.Activity;
 import android.app.ActivityManager;
 import android.content.Context;
+import android.net.Uri;
 import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -17,6 +18,7 @@ import com.google.ar.core.Plane;
 import com.google.ar.sceneform.AnchorNode;
 import com.google.ar.sceneform.math.Vector3;
 import com.google.ar.sceneform.rendering.Color;
+import com.google.ar.sceneform.rendering.Material;
 import com.google.ar.sceneform.rendering.MaterialFactory;
 import com.google.ar.sceneform.rendering.ModelRenderable;
 import com.google.ar.sceneform.rendering.ShapeFactory;
@@ -29,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
     private static final double MIN_OPENGL_VERSION = 3.0;
     ArFragment arFragment;
     ModelRenderable sphere;
+
     boolean hasSpawned = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +45,8 @@ public class MainActivity extends AppCompatActivity {
         Toast t = Toast.makeText(this, "Looking for Plane Surface!", Toast.LENGTH_LONG);
         t.setGravity(Gravity.CENTER, 0, 0);
         t.show();
+
+
         MaterialFactory.makeOpaqueWithColor(this, new Color(android.graphics.Color.RED))
                 .thenAccept(
                         material -> {
@@ -53,6 +58,10 @@ public class MainActivity extends AppCompatActivity {
             return null;
         });
 
+
+
+
+
         arFragment.setOnTapArPlaneListener((HitResult hitResult, Plane plane, MotionEvent motionEvent) ->
         {
             if (sphere == null)
@@ -62,19 +71,30 @@ public class MainActivity extends AppCompatActivity {
 
             if (!hasSpawned)
             {
-                Anchor anchor = hitResult.createAnchor();
-                AnchorNode anchorNode = new AnchorNode(anchor);
-                anchorNode.setParent(arFragment.getArSceneView().getScene());
-
-                TransformableNode s = new TransformableNode(arFragment.getTransformationSystem());
-                s.setParent(anchorNode);
-                s.setRenderable(sphere);
-                s.select();
+                spawnSphere(hitResult, sphere);
+                /*ModelRenderable sphere2 = sphere.makeCopy();
+                sphere2.getMaterial().setFloat3(
+                        MaterialFactory.MATERIAL_COLOR, new Color(android.graphics.Color.BLUE));
+                spawnSphere(hitResult, sphere2);*/
                 hasSpawned = true;
+
             }
 
 
         });
+
+    }
+    public boolean spawnSphere(HitResult hitResult, ModelRenderable sph)
+    {
+        Anchor anchor = hitResult.createAnchor();
+        AnchorNode anchorNode = new AnchorNode(anchor);
+        anchorNode.setParent(arFragment.getArSceneView().getScene());
+
+        TransformableNode s = new TransformableNode(arFragment.getTransformationSystem());
+        s.setParent(anchorNode);
+        s.setRenderable(sph);
+        s.select();
+        return true;
 
     }
 
