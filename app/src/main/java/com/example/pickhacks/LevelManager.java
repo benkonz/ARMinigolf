cf8318d682b6df411d1c955b9acaa9774ddab0f7
Brandon Lacy <blacy1551@gmail.com>
2019-03-03 01:34:29 -0600

    Added Level Manager and 2nd level

---
+++ app/src/main/java/com/example/pickhacks/LevelManager.java
@@ -0,0 +1,56 @@
+package com.example.pickhacks;
+
+import com.google.ar.sceneform.collision.Box;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LevelManager
+{
+    private List levels;
+    private Box[] currentLevel;
+    private int levelNum = 0;
+
+    public LevelManager()
+    {
+        levels = new ArrayList<Box[]>();
+
+    }
+
+    public void IncreaseLevel()
+    {
+        levelNum++;
+
+        currentLevel = (Box[])levels.get(levelNum - 1);
+    }
+    public void DecreaseLevel()
+    {
+        levelNum--;
+        if (levelNum <= 0)
+        {
+            levelNum = levels.size();
+        }
+        currentLevel = (Box[])levels.get(levelNum - 1);
+    }
+    public List<Box[]> getAllLevels()
+    {
+        return levels;
+    }
+    public Box[] getLevel(int index)
+    {
+        return (Box[])levels.get(index);
+    }
+    public Box[] getCurrentLevel()
+    {
+        return currentLevel;
+    }
+    public void addLevel(Box[] level)
+    {
+        levels.add(level);
+    }
+
+
+
+
+
+}
